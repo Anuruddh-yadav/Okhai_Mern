56add96ee8f0d0e039a94d11ccdc3c6da83a82a2
anuruddhyadav5135939-cloud <anuruddh.yadav.5135939@ves.ac.in>
2026-01-29 13:54:06 +0530

    Add frontend and backend

---
+++ Backend/Db.js
@@ -0,0 +1,2 @@
+const data =["apple", "banana", "cherry", "date", "elderberry"];
+module.exports = data;  
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-green-600 mt-10 pt-10 '>Welcome to the App</h1>
+    </div>
+  )
+}
+
+export default App
